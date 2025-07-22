ae5e32bb1c45d865293acc3dcd261da894a2e30f
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2025-07-22 11:48:26 +0100

    fix: ibmcloud cli install

--- modules/tfe-install/main.tf
+++ modules/tfe-install/main.tf
@@ -328,9 +328,7 @@ resource "kubectl_manifest" "tfe_agent_build_config" {
           # Install the ibmcloud CLI & all plugins
           RUN curl -fsSL https://clis.cloud.ibm.com/install/linux | sh && \
           ibmcloud config --check-version=false && \
-          ibmcloud api cloud.ibm.com && \
-          chmod +x /usr/local/bin/ibmcloud && \
-          chown -R 777 /usr/local/bin/ibmcloud && \
+          chmod +x $(which ibmcloud) && \
           ibmcloud plugin install -a -f
 
           USER tfc-agent
