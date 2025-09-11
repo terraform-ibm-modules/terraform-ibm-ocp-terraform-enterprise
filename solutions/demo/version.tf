92e7d6a39019745f672bab3f9832011c88837293
Todd Giguere <todd.giguere@ibm.com>
2025-09-11 17:10:58 -0400

    fix: provider version to avoid bug

--- solutions/demo/version.tf
+++ solutions/demo/version.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.82.0"
+      version = "1.82.1"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
