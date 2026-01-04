d7c8b1a482a11bd4cac73a4a6e18133cf221d388
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-04 21:27:09 +0000

    fix(deps): update dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -48,7 +48,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.0"
+  version = "1.4.7"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.existing_resource_group_name == null ? "${local.prefix}resource-group" : null
   existing_resource_group_name = var.existing_resource_group_name
--- modules/ocp-vpc/main.tf
+++ modules/ocp-vpc/main.tf
@@ -4,7 +4,7 @@
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.10.0"
+  version           = "8.10.4"
   resource_group_id = var.resource_group_id
   region            = var.region
   create_vpc        = var.existing_vpc_id == null ? true : false
@@ -80,7 +80,7 @@ locals {
 module "openshift" {
   count                               = var.existing_cluster_id == null ? 1 : 0
   source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                             = "3.74.0"
+  version                             = "3.76.3"
   cluster_name                        = var.cluster_name
   resource_group_id                   = var.resource_group_id
   region                              = var.region
