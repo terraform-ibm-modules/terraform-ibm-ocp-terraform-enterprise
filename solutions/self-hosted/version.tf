5a06d4cecb02cd41877c9960a17c6444f4f890d3
shemau <shemau@gmail.com>
2026-01-06 12:21:10 +0000

    fix(deps): avoid kubernetes v3 provider

--- solutions/self-hosted/version.tf
+++ solutions/self-hosted/version.tf
@@ -14,7 +14,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "3.0.1"
+      version = "2.38.0"
     }
     helm = {
       source  = "hashicorp/helm"
