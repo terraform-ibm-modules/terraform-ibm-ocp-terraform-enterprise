13e952e42550f80a8921f9bbb743acdec70873cd
Valerio Bontempi <valerio.bontempi@it.ibm.com>
2026-03-23 12:54:57 +0100

    fix: fix renovate and dependencies

--- main.tf
+++ main.tf
@@ -66,9 +66,9 @@ module "cos" {
   bucket_name              = var.cos_bucket_name
   add_bucket_name_suffix   = true
   create_cos_bucket        = true
-  retention_enabled        = var.cos_retention # disable retention for test environments - enable for stage/prod
-  kms_encryption_enabled   = true
-  kms_key_crn              = module.key_protect_all_inclusive.keys["terraform-enterprise.terraform-enterprise-cos"].crn
+  # retention_enabled        = var.cos_retention # disable retention for test environments - enable for stage/prod
+  kms_encryption_enabled = true
+  kms_key_crn            = module.key_protect_all_inclusive.keys["terraform-enterprise.terraform-enterprise-cos"].crn
   resource_keys = [
     {
       name                      = "tfe-credentials"
@@ -116,9 +116,12 @@ module "icd_postgres" {
   use_same_kms_key_for_backups = false
   kms_key_crn                  = module.key_protect_all_inclusive.keys["terraform-enterprise.terraform-enterprise-postgresql"].crn
   backup_encryption_key_crn    = module.key_protect_all_inclusive.keys["terraform-enterprise.terraform-enterprise-postgresql-backup"].crn
-  service_credential_names = {
-    "tfe" : "Operator"
-  }
+  service_credential_names = [
+    {
+      "name" : "tfe",
+      "role" : "Operator"
+    }
+  ]
   deletion_protection = var.postgres_deletion_protection
 }
 
--- variables.tf
+++ variables.tf
@@ -180,11 +180,11 @@ variable "cos_bucket_name" {
   default     = "tfe-cos-bucket"
 }
 
-variable "cos_retention" {
-  type        = bool
-  description = "Whether retention for the Object Storage bucket is enabled. Enable for staging and prod environments."
-  default     = false
-}
+# variable "cos_retention" {
+#   type        = bool
+#   description = "Whether retention for the Object Storage bucket is enabled. Enable for staging and prod environments."
+#   default     = false
+# }
 
 ##############################################################################
 # PostGres
