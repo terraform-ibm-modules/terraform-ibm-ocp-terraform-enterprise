470bd9ac3d7c590cd5b27c1338000d4bdf8d2946
Piyush Kumar Sahu <Piyush.Kumar.Sahu@ibm.com>
2026-06-10 13:38:38 +0530

    updated the tf version

--- examples/complete/version.tf
+++ examples/complete/version.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.88.0"
+      version = "2.1.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
